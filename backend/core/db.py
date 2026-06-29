752094e0d369861d0c08445a27c9efe037dc2c54
Cauan Nascimento <caunas1337@proton.me>
2026-06-29 16:13:03 -0300

    add: Conexão com o banco de dados

---
+++ backend/core/db.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+
+DATABASE_URL = "sqlite:///./db.sqlite"
+
+engine = create_engine(
+    DATABASE_URL,
+    echo = False,
+    connect_args = {"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    bind = engine,
+    autoflush=False,
+    autocommit=False
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+def create_db():
+    Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
