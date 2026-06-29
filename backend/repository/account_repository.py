4bae87bae74663986eed761837043b02b3dcd0e8
Cauan Nascimento <caunas1337@proton.me>
2026-06-29 18:36:31 -0300

    fix: Missing self arguments

--- backend/repository/account_repository.py
+++ backend/repository/account_repository.py
@@ -1,27 +1,25 @@
-from sqlalchemy.orm import Session
-
-from app.models.account import Account
+from models.account import Account
 
 
 class AccountRepository:
     # Recebe a sessão do database no construtor
-    def __init__(self, db: Session):
+    def __init__(self, db):
         self.db = db
     
     def getById(self, account_id, int):
         return self.db.get(Account, account_id)
 
-    def save():
+    def save(self, account: Account):
         self.db.add(account)
         self.db.commit()
         self.db.refresh(account)
         return account
 
-    def update():
+    def update(self):
         self.db.commit()
         self.db.refresh()
         return account
 
-    def delete():
+    def delete(self):
         db.delete(account)
         db.commit()
