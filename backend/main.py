73eeee7ea0e5bbd411b969994785e61304fac802
Cauan Nascimento <caunas1337@proton.me>
2026-06-29 18:40:37 -0300

    add: Trace account_routes

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from core.db import create_db
+from router.account_router import router as account_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db()
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/ping")
 def ping():
     return "pong"
+
+app.include_router(
+    account_router,
+    prefix="/api"
+)
