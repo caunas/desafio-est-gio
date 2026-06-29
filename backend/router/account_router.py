9f633a22383e2cfe235bc43e1d9ef6a4750a1dbe
Cauan Nascimento <caunas1337@proton.me>
2026-06-29 18:38:14 -0300

    add: New account route

---
+++ backend/router/account_router.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends
+
+from models.checking import CheckingAccount
+from models.savings import SavingsAccount
+from service.account_service import *
+from schemas.account_schema import CheckingAccountCreate, SavingsAccountCreate, AccountResponse
+from repository.account_repository import AccountRepository
+from core.db import get_db
+
+router = APIRouter(
+    prefix="/account",
+    tags=["Account"]
+)
+
+# Injetar dependências dessa forma escala melhor
+def get_account_service(
+    db = Depends(get_db)
+) -> AccountService:
+    repository = AccountRepository(db)
+    return AccountService(repository)
+
+
+@router.post("/checking", 
+response_model = AccountResponse, 
+status_code = 201)
+def create_checking_account(
+    data: CheckingAccountCreate,
+    db = Depends(get_db),
+    service: AccountService = Depends(get_account_service)
+):
+
+    return service.create_checking_account(
+        holder_name=data.holder_name,
+        initial_balance=data.initial_balance,
+    )
